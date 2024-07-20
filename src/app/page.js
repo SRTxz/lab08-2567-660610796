eecfc75569f4cf372e65212f704dbe36d28b8d2e
Toey <sarantoey8@gmail.com>
2024-07-20 16:26:57 +0700

    completed

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import Task from "./components/task";
+import Task from "./components/Task";
 import Taskinput from "./components/Taskinput";
 
 export default function Home() {
