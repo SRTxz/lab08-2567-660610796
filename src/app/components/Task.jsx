30c9b8b1ff06beeca69508350439225770163da5
Toey <sarantoey8@gmail.com>
2024-07-20 16:25:26 +0700

    completed?

--- src/app/components/Task.jsx
+++ src/app/components/Task.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export default function Task() {
+export default function Task({task}) {
   return (
     <div className="d-flex p-3 gap-2 align-items-center border-bottom">
-          <span style={{ textDecoration: Task.isDone ? "line-through" :"none"}}>{Task.title}</span>
+          <span style={{ textDecoration: task.isDone ? "line-through" :"none"}}>{task.title}</span>
           <button className="btn btn-success">Done</button>
           <button className="btn btn-danger">Delete</button>
     </div>
